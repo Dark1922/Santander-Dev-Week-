8bcea00fad7b793ec184d38d4a57fa13941d6a03
dark1922 <jvdematos004@gmail.com>
2021-05-29 01:29:52 -0300

    put

--- bootcampBackEnd/src/main/java/controller/StockController.java
+++ bootcampBackEnd/src/main/java/controller/StockController.java
@@ -1,8 +1,10 @@
 package controller;
 
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +22,12 @@ public class StockController {
 		return ResponseEntity.ok(dto);
 		
 	} 
-	
-  
+	//pq ta fazendo alteração, ta construindo um json e retornando um json
+     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)  
+	public ResponseEntity<StockDTO> update(@RequestBody StockDTO dto) {
+		return ResponseEntity.ok(dto);
+	}
+     
+     //trazer tudo que tá cadastrado
 
 }
