4f4f5b785714e37b03aca1f4d1cf85baea515c39
dark1922 <jvdematos004@gmail.com>
2021-05-29 01:56:36 -0300

    feat: GetMapping value id

--- bootcampBackEnd/src/main/java/controller/StockController.java
+++ bootcampBackEnd/src/main/java/controller/StockController.java
@@ -1,8 +1,15 @@
 package controller;
 
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,5 +36,44 @@ public class StockController {
 	}
      
      //trazer tudo que tá cadastrado
+     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+     public ResponseEntity<List<StockDTO>> findAll(){//pega todo mundo e retorna
+    	 
+    	 List<StockDTO> list = new ArrayList<>();
+    	 StockDTO dto = new StockDTO();
+    	 dto.setId(1L);//L do lado pq é do tipo long
+    	 dto.setName("magazine luiza");
+    	 dto.setPrice(90.99);
+    	 dto.setDate(LocalDate.now());//dia de agora atualmente
+    	 list.add(dto); //adicionando a lista
+		return ResponseEntity.ok(list);
+				
+    	 
+     }
+   //pegando algo getmapping//pega um id especifico 
+ 		@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)  
+      public ResponseEntity<StockDTO> findById(@PathVariable Long id) {//responsavel pegar id
+ 			 List<StockDTO> list = new ArrayList<>();
+ 	    	 StockDTO stock1= new StockDTO();
+ 	    	stock1.setId(1L);//L do lado pq é do tipo long
+ 	    	stock1.setName("magazine luiza");
+ 	    	stock1.setPrice(90.99);
+ 	    	stock1.setDate(LocalDate.now());//dia de agora atualmente
+ 	    	 list.add(stock1); //adicionando a lista
+ 				
+ 			 
+ 	    	 StockDTO stock2 = new StockDTO();
+ 	    	stock2.setId(2L);//L do lado pq é do tipo long
+ 	    	stock2.setName("Ponto Frio");
+ 	    	stock2.setPrice(7000.00);
+ 	    	stock2.setDate(LocalDate.now());//dia de agora atualmente
+ 	    	 list.add(stock2); //adicionando a lista
+ 	    	 
+ 	    StockDTO dtoselect = list.stream().filter(x -> x.getId().compareTo(id) == 0).findFirst().get();
+ 	    return ResponseEntity.ok(dtoselect);
+ 			
+    			 
+    	 
+     }
 
 }
